5376923ba578adc6accbb6e031d8efb51399a51c
Ashley Burns <Ash_burn999@yahoo.com>
2021-10-14 23:34:50 -0400

    seed file made and ran

---
+++ data/seeds/01-bible-seed.js
@@ -0,0 +1,239 @@
+exports.seed = function (knex) {
+    return knex('bible').insert([
+        {
+            bible_id: 1,
+            book: 'Genesis',
+            chapter: 1,
+            verse: 1,
+            passage: 'In the beginning God created the heavens and the earth.',
+            version: 'New Living Translation'
+        },
+        {
+            bible_id: 2,
+            book: 'Genesis',
+            chapter: 1,
+            verse: 2,
+            passage: 'The earth was formless and empty, and darkness covered the deep waters. And the Spirit of God was hovering over the surface of the waters.',
+            version: 'New Living Translation'
+        },
+        {
+            bible_id: 3,
+            book: 'Genesis',
+            chapter: 1,
+            verse: 3,
+            passage: 'Then God said, “Let there be light,” and there was light.',
+            version: 'New Living Translation'
+        },
+        {
+            bible_id: 4,
+            book: 'Genesis',
+            chapter: 1,
+            verse: 4,
+            passage: 'And God saw that the light was good. Then he separated the light from the darkness.',
+            version: 'New Living Translation'
+        },
+        {
+            bible_id: 5,
+            book: 'Genesis',
+            chapter: 1,
+            verse: 5,
+            passage: 'God called the light “day” and the darkness “night.”. And evening passed and morning came, marking the first day.',
+            version: 'New Living Translation'
+        },
+        {
+            bible_id: 6,
+            book: 'Genesis',
+            chapter: 1,
+            verse: 6,
+            passage: 'Then God said, “Let there be a space between the waters, to separate the waters of the heavens from the waters of the earth.”',
+            version: 'New Living Translation'
+        },
+        {
+            bible_id: 7,
+            book: 'Genesis',
+            chapter: 1,
+            verse: 7,
+            passage: 'And that is what happened. God made this space to separate the waters of the earth from the waters of the heavens.',
+            version: 'New Living Translation'
+        },
+        {
+            bible_id: 8,
+            book: 'Genesis',
+            chapter: 1,
+            verse: 8,
+            passage: 'God called the space “sky.”. And evening passed and morning came, marking the second day.',
+            version: 'New Living Translation'
+        },
+        {
+            bible_id: 9,
+            book: 'Genesis',
+            chapter: 1,
+            verse: 9,
+            passage: 'Then God said, “Let the waters beneath the sky flow together into one place, so dry ground may appear.” And that is what happened.',
+            version: 'New Living Translation'
+        },
+        {
+            bible_id: 10,
+            book: 'Genesis',
+            chapter: 1,
+            verse: 10,
+            passage: 'God called the dry ground “land” and the waters “seas.” And God saw that it was good.',
+            version: 'New Living Translation'
+        }, {
+            bible_id: 11,
+            book: 'Genesis',
+            chapter: 1,
+            verse: 11,
+            passage: 'Then God said, “Let the land sprout with vegetation—every sort of seed-bearing plant, and trees that grow seed-bearing fruit. These seeds will then produce the kinds of plants and trees from which they came.” And that is what happened.',
+            version: 'New Living Translation'
+        }, {
+            bible_id: 12,
+            book: 'Genesis',
+            chapter: 1,
+            verse: 12,
+            passage: 'The land produced vegetation—all sorts of seed-bearing plants, and trees with seed-bearing fruit. Their seeds produced plants and trees of the same kind. And God saw that it was good.',
+            version: 'New Living Translation'
+        }, {
+            bible_id: 13,
+            book: 'Genesis',
+            chapter: 1,
+            verse: 13,
+            passage: 'And evening passed and morning came, marking the third day.',
+            version: 'New Living Translation'
+        }, {
+            bible_id: 14,
+            book: 'Genesis',
+            chapter: 1,
+            verse: 14,
+            passage: 'Then God said, “Let lights appear in the sky to separate the day from the night. Let them be signs to mark the seasons, days, and years.',
+            version: 'New Living Translation'
+        }, {
+            bible_id: 15,
+            book: 'Genesis',
+            chapter: 1,
+            verse: 15,
+            passage: 'Let these lights in the sky shine down on the earth.” And that is what happened.',
+            version: 'New Living Translation'
+        }, {
+            bible_id: 16,
+            book: 'Genesis',
+            chapter: 1,
+            verse: 16,
+            passage: 'God made two great lights—the larger one to govern the day, and the smaller one to govern the night. He also made the stars.',
+            version: 'New Living Translation'
+        }, {
+            bible_id: 17,
+            book: 'Genesis',
+            chapter: 1,
+            verse: 17,
+            passage: 'God set these lights in the sky to light the earth,',
+            version: 'New Living Translation'
+        }, {
+            bible_id: 18,
+            book: 'Genesis',
+            chapter: 1,
+            verse: 18,
+            passage: 'to govern the day and night, and to separate the light from the darkness. And God saw that it was good.',
+            version: 'New Living Translation'
+        }, {
+            bible_id: 19,
+            book: 'Genesis',
+            chapter: 1,
+            verse: 19,
+            passage: 'And evening passed and morning came, marking the fourth day.',
+            version: 'New Living Translation'
+        }, {
+            bible_id: 20,
+            book: 'Genesis',
+            chapter: 1,
+            verse: 20,
+            passage: 'Then God said, “Let the waters swarm with fish and other life. Let the skies be filled with birds of every kind.” ',
+            version: 'New Living Translation'
+        }, {
+            bible_id: 21,
+            book: 'Genesis',
+            chapter: 1,
+            verse: 21,
+            passage: 'So God created great sea creatures and every living thing that scurries and swarms in the water, and every sort of bird—each producing offspring of the same kind. And God saw that it was good',
+            version: 'New Living Translation'
+        }, {
+            bible_id: 22,
+            book: 'Genesis',
+            chapter: 1,
+            verse: 22,
+            passage: 'Then God blessed them, saying, “Be fruitful and multiply. Let the fish fill the seas, and let the birds multiply on the earth.”',
+            version: 'New Living Translation'
+        }, {
+            bible_id: 23,
+            book: 'Genesis',
+            chapter: 1,
+            verse: 23,
+            passage: 'And evening passed and morning came, marking the fifth day',
+            version: 'New Living Translation'
+        }, {
+            bible_id: 24,
+            book: 'Genesis',
+            chapter: 1,
+            verse: 24,
+            passage: 'Then God said, “Let the earth produce every sort of animal, each producing offspring of the same kind—livestock, small animals that scurry along the ground, and wild animals.” And that is what happened.',
+            version: 'New Living Translation'
+        }, {
+            bible_id: 25,
+            book: 'Genesis',
+            chapter: 1,
+            verse: 25,
+            passage: 'God made all sorts of wild animals, livestock, and small animals, each able to produce offspring of the same kind. And God saw that it was good.',
+            version: 'New Living Translation'
+        }, {
+            bible_id: 26,
+            book: 'Genesis',
+            chapter: 1,
+            verse: 26,
+            passage: 'Then God said, “Let us make human beings in our image, to be like us. They will reign over the fish in the sea, the birds in the sky, the livestock, all the wild animals on the earth, and the small animals that scurry along the ground.”',
+            version: 'New Living Translation'
+        }, {
+            bible_id: 27,
+            book: 'Genesis',
+            chapter: 1,
+            verse: 27,
+            passage: 'So God created human beings in his own image.In the image of God he created them; male and female he created them.',
+            version: 'New Living Translation'
+        }, {
+            bible_id: 28,
+            book: 'Genesis',
+            chapter: 1,
+            verse: 28,
+            passage: 'Then God blessed them and said, “Be fruitful and multiply. Fill the earth and govern it. Reign over the fish in the sea, the birds in the sky, and all the animals that scurry along the ground.”',
+            version: 'New Living Translation'
+        }, {
+            bible_id: 29,
+            book: 'Genesis',
+            chapter: 1,
+            verse: 29,
+            passage: 'Then God said, “Look! I have given you every seed-bearing plant throughout the earth and all the fruit trees for your food.',
+            version: 'New Living Translation'
+        }, {
+            bible_id: 30,
+            book: 'Genesis',
+            chapter: 1,
+            verse: 30,
+            passage: 'And I have given every green plant as food for all the wild animals, the birds in the sky, and the small animals that scurry along the ground—everything that has life.” And that is what happened.',
+            version: 'New Living Translation'
+        }, {
+            bible_id: 31,
+            book: 'Genesis',
+            chapter: 1,
+            verse: 31,
+            passage: 'Then God looked over all he had made, and he saw that it was very good! And evening passed and morning came, marking the sixth day.',
+            version: 'New Living Translation'
+        },
+        {
+            bible_id: 32,
+            book: 'Genesis',
+            chapter: 2,
+            verse: 1,
+            passage: 'So the creation of the heavens and the earth and everything in them was completed.',
+            version: 'New Living Translation'
+        }
+    ]);
+}
