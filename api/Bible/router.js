ac7b7cd361919bb6b5593361fbde8c3e73a2ddfb
Ashley Burns <ash_burn999@yahoo.com>
2021-11-25 16:30:42 -0500

    trying to fix edit

--- api/Bible/router.js
+++ api/Bible/router.js
@@ -1,46 +1,57 @@
-const express = require('express')
-const Bible = require('./model')
-const router = express.Router()
+const express = require('express');
+const Bible = require('./model');
+const router = express.Router();
 
 router.get('/', (req, res, next) => {
     Bible.getAll()
         .then(bibleVerses => res.status(200).json(bibleVerses))
         .catch(err => {
-            next(err)
-        })
-})
+            next(err);
+        });
+});
 router.get('/:book', (req, res, next) => {
-    const {book} = req.params
-    Bible.findByBook({book})
+    const { book } = req.params;
+    Bible.findByBook({ book })
         .then(bbook => res.status(200).json(bbook))
-        .catch(err=>{
-            next(err)
-        })
-})
+        .catch(err => {
+            next(err);
+        });
+});
 router.get('/:book/:chapter', (req, res, next) => {
-    const {book, chapter} = req.params
-    Bible.findByBookChapter({book, chapter})
-        .then(info => res.status(200). json(info))
-        .catch(err=>{
-            next(err)
-        })
-})
+    const { book, chapter } = req.params;
+    Bible.findByBookChapter({ book, chapter })
+        .then(info => res.status(200).json(info))
+        .catch(err => {
+            next(err);
+        });
+});
 
 router.get('/:book/:chapter/:verse', (req, res, next) => {
-    const {book, chapter, verse} = req.params
-    Bible.findByChapterVerse({book, chapter, verse})
-        .then(passage => res.status(200). json(passage))
-        .catch(err=>{
-            next(err)
-        })
-})
+    const { book, chapter, verse } = req.params;
+    Bible.findByChapterVerse({ book, chapter, verse })
+        .then(passage => res.status(200).json(passage))
+        .catch(err => {
+            next(err);
+        });
+});
 
 router.post('/', (req, res, next) => {
     Bible.add(req.body)
         .then(newVerse => res.status(201).json(newVerse))
-        .catch(err=>{
-            next(err)
+        .catch(err => {
+            next(err);
+        });
+});
+
+router.put('/:book/:chapter/:verse', (req, res, next) => {
+    const { book, chapter, verse } = req.params;
+    Bible.edit({ book, chapter, verse })
+        .then(editVerse => {
+            res.json(editVerse);
         })
+        .catch(err => {
+            next(err);
+        });
 });
 
 module.exports = router;
